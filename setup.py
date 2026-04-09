df1ac8edffa4178106e51f3ffc2e8531ca8e304d
Bui Son Tung <sontungbui6942@gmail.com>
2026-04-10 00:29:05 +0700

    setup.py updated

--- setup.py
+++ setup.py
@@ -1,24 +1,27 @@
 from setuptools import find_packages, setup
 from typing import List
 
-HYPHEN_E_DOT = '-e .'
+HYPHEN_E_DOT = "-e ."
 
-def get_requirements(file_path: str) ->List[str]:
-    requirements = []
+
+def get_requirements(file_path: str) -> List[str]:
+    requirements: List[str] = []
     with open(file_path) as file_obj:
-        requirements=file_obj.readlines()
-        requirements=[req.replace("\n", " ") for req in requirements]
+        requirements = file_obj.readlines()
+        requirements = [req.replace("\n", "").strip() for req in requirements]
 
-        if HYPHEN_E_DOT in requirements:
-            requirements.remove(HYPHEN_E_DOT)
+    if HYPHEN_E_DOT in requirements:
+        requirements.remove(HYPHEN_E_DOT)
 
     return requirements
-    
+
+
 setup(
-    name='video-action-recognition',
-    version='0.0.1',
-    author='Bui Son Tung',
-    author_email='sontungbui6942@gmail.com',
-    packages=find_packages(),
-    install_requires=get_requirements('requirements.txt')
+    name="video-action-recognition",
+    version="0.0.1",
+    author="Bui Son Tung",
+    author_email="sontungbui6942@gmail.com",
+    package_dir={"": "src"},
+    packages=find_packages(where="src"),
+    install_requires=get_requirements("requirements.txt"),
 )
